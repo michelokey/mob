c1faee4ed604c3c92fa93f51048b775737c9f2be
michelokey <moi_c_mishel@yahoo.fr>
2014-08-26 14:24:10 +0000

    neww

--- js/jss.js
+++ js/jss.js
@@ -20,3 +20,23 @@ $(function() {
 function hideSplash() {
   $.mobile.changePage("#home", "fade");
 }
+
+ 
+
+            $(document).ready(function() {
+                // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
+                $(window).resize(function() {
+                    $("#monbloc").css({
+                        position:'absolute',
+                        left:($(window).width() - $("#monbloc").outerWidth()) / 2,
+                        top:($(window).height() - $("#monbloc").outerHeight()) / 2
+                    });
+                });
+            });
+
+            $(window).load(function() {
+                // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
+                $(window).resize();
+            });
+
+      
