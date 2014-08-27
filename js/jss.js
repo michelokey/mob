0da64b5effe2982526bce71b22ba845414a8410c
michelokey <moi_c_mishel@yahoo.fr>
2014-08-27 09:54:41 +0000

    ne

--- js/jss.js
+++ js/jss.js
@@ -1,5 +1,21 @@
 // JavaScript Document
+  $(document).ready(function() {
+                // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
+                $(window).resize(function() {
+                    $("#monbloc").css({
+                        position:'absolute',
+                        left:($(window).width() - $("#monbloc").outerWidth()) / 2,
+                        top:($(window).height() - $("#monbloc").outerHeight()) / 2
+                    });
+                });
+				$.mobile.page.prototype.options.domCache = true;
+            });
 
+            $(window).load(function() {
+                // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
+                $(window).resize();
+            });
+			
 $(document).on('pageinit',function(){
 	$("#home").on("swiperight",function(){
 		$("#leftpanel").panel( "open");
@@ -23,20 +39,6 @@ function hideSplash() {
 
  
 
-            $(document).ready(function() {
-                // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
-                $(window).resize(function() {
-                    $("#monbloc").css({
-                        position:'absolute',
-                        left:($(window).width() - $("#monbloc").outerWidth()) / 2,
-                        top:($(window).height() - $("#monbloc").outerHeight()) / 2
-                    });
-                });
-            });
-
-            $(window).load(function() {
-                // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
-                $(window).resize();
-            });
+          
 
       
