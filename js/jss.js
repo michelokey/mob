5ace6c9504c04efd8a40c00c6719860316fad5c9
michelokey <michel@undeplus.net>
2014-08-27 10:22:13 +0000

    Update jss.js

--- js/jss.js
+++ js/jss.js
@@ -8,10 +8,7 @@
                         top:($(window).height() - $("#monbloc").outerHeight()) / 2
                     });
                 });
-<<<<<<< HEAD
-				$.mobile.page.prototype.options.domCache = true;
-=======
->>>>>>> origin/master
+				 
             });
 
             $(window).load(function() {
