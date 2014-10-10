e31ca8bee675a470d13f5546bfdfe3a37ffc3cab
michelokey <moi_c_mishel@yahoo.fr>
2014-10-10 11:27:02 +0000

    ins

---
+++ js - Copie/jss.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+
+$(document).on('pageinit',function(){
+	$("#home").on("swiperight",function(){
+		$("#leftpanel").panel( "open");
+	});
+	$("#home").on("swipeleft",function(){
+		$("#rightpanel").panel( "open");
+	});
+	$("#home").on("swipeleft",function(){
+		$("#about").panel( "open");
+	});
+	
+});
+
+$(function() {
+  setTimeout(hideSplash, 5000);
+});
+
+function hideSplash() {
+  $.mobile.changePage("#home", "fade");
+}
+
+ 
+
+            $(document).ready(function() {
+                // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
+                $(window).resize(function() {
+                    $("#monbloc").css({
+                        position:'absolute',
+                        left:($(window).width() - $("#monbloc").outerWidth()) / 2,
+                        top:($(window).height() - $("#monbloc").outerHeight()) / 2
+                    });
+                });
+            });
+
+            $(window).load(function() {
+                // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
+                $(window).resize();
+            });
+
+      
---
+++ js/jss.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+  $(document).ready(function() {
+                // $(window).resize() est appelée chaque fois que la fenêtre est redimensionnée par l'utilisateur.
+                $(window).resize(function() {
+                    $("#monbloc").css({
+                        position:'absolute',
+                        left:($(window).width() - $("#monbloc").outerWidth()) / 2,
+                        top:($(window).height() - $("#monbloc").outerHeight()) / 2
+                    });
+                });
+				 
+            });
+
+            $(window).load(function() {
+                // au chargement complet de la page, la fonction resize() est appelée une fois pour initialiser le centrage.
+                $(window).resize();
+            });
+			
+$(document).on('pageinit',function(){
+	$("#home").on("swiperight",function(){
+		$("#leftpanel").panel( "open");
+	});
+	$("#home").on("swipeleft",function(){
+		$("#rightpanel").panel( "open");
+	});
+	$("#home").on("swipeleft",function(){
+		$("#about").panel( "open");
+	});
+	
+});
+
+$(function() {
+  setTimeout(hideSplash, 5000);
+});
+
+function hideSplash() {
+  $.mobile.changePage("home.php#home", "fade");
+}
+
+ 
+$(function(){
+    $('#inner-content-div').slimScroll({
+        height: 'auto'
+    });
+}); 
+
+$(function(){
+    $('#inner-content-div2').slimScroll({
+        height: 'auto'
+    });
+});    
+
+$(function(){
+    $('#inner-content-div3').slimScroll({
+        height: 'auto',
+		 
+    });
+});
+   
+
+      
